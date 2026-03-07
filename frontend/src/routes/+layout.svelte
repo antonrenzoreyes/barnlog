6795389f0155358c11674ce9cbe24584adbb1e5d
Anton-Renzo Reyes <antonrenzoreyes@gmail.com>
2026-03-08 00:37:01 +0800

    chore(frontend): isolate Svelte lint/format config
    Refs #70

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-  import './layout.css';
-  import favicon from '$lib/assets/favicon.svg';
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
 
   const { children } = $props();
 </script>
 
-<svelte:head><link
-  href={favicon}
-  rel="icon"
-/></svelte:head>
+<svelte:head><link href={favicon} rel="icon" /></svelte:head>
 {@render children()}
